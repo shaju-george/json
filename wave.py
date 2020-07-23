db11e86ca9139cf73061dba8c7d0d0acc6131f36
shaju george <shajugeorge45@gmail.com>
2020-07-23 18:10:19 +0530

    q5 done

--- wave.py
+++ wave.py
@@ -1,20 +1,17 @@
 import json
+from collections import Counter
 
 today=[]
 
 for line in open('today.json.gz_out','r'):
     today.append(json.loads(line))
-'''
-with open("today.json.gz_out",'a') as f:
-    for line in f:
-        today.append(json.load(line))
-''' '''
-dictionay = { "t" : today}
-print(dictionay)
-'''
+
 # print(today[0]['urlh'])
 
+
+
 u_set = set()
+
 for line in today:
    
     #for URLH in  line['urlh']:
@@ -30,10 +27,7 @@ us = len(u_set)
 #if '90d8967979b37a682553c116629e86f7f2c4efbb' in u:
 #    a+=1
 #print(a)
-'''
-print(len(u))
-print(u)
-'''  
+
 u_list = []
 for line in today:
     u_list.append(line['urlh'])
@@ -70,34 +64,14 @@ for i in u_set:
                 pass
             
         '''
-
-#print(len(overlapping))
-
-#print(over)
-#print("number of URLH which are overlapping between today.json and yesterday.json = " ,over)
 print("1) number of URLH which are overlapping between today.json and yesterday.json = " ,len(overlapping))
 
-over = u_set.intersection(y_set)
-print(len(over))
-'''
-y_list = list()
-for line in yesterday:
-    y_list.append(line['urlh'])
-#print(len(y_set))
+# or
+#over = u_set.intersection(y_set)
+#print(len(over))
 
-
-
-
-over = 0
-for i in u_list:
-    if i in y_list :
-        over+=1
-
-#print(over)
-print("number of URLH which are overlapping between today.json and yesterday.json when repetation is allowed = " ,over)
-
-'''
-
+print(":) __________________________________ :)")
+print()
 '''
 diff=0.0
 for line in today:
@@ -112,3 +86,85 @@ for line in today:
 print(diff)
 '''          
 #some where 'available_price' is returning None type!
+
+#3 No of Unique categories in both files.
+
+cata_t = set()
+subcategory_t =[]
+for line in today:
+    cata_t.add(line['category'])
+    subcategory_t.append(line['subcategory'])
+print(' No of Unique categories in today.json ',len(cata_t))
+
+cata_y = set()
+subcategory_y =[]
+for line in yesterday:
+    cata_y.add(line['category'])
+    subcategory_y.append(line['subcategory'])
+print(' No of Unique categories in yesterday.json ',len(cata_y))
+
+
+unique = cata_t
+for category in cata_y:
+    unique.add(category)
+print(' 3) No of Unique categories in both files ',len(unique))
+
+print(" :) __________________________________ :)")
+print()
+
+#4
+cata_tlist= list(cata_t)
+cata_ylist=list(cata_y)
+#unique = list(unique)
+
+cata_over = cata_y.intersection(cata_t)
+
+for category in cata_over:
+    unique.remove(category)
+print('  4) List of categories which is not overlapping = ',len(unique))
+print("  :) __________________________________ :)")
+print()
+
+#5. Generate the stats with count for all taxonomies 
+
+'''
+
+today_new= []
+for dic in today:
+    today_new= ([key,val] for key, val in dic.items() if key =='subcategory' )
+print(today_new[0])
+Counter(today_new)
+#print(json.dumps(today_new,indent=0,separators=(">","=")))
+#print(type(today_new))
+'''
+'''
+for i in today_new[0:3]:
+    today.remove
+    (json.dumps(i,indent=0,separators=(">","=")))
+ 
+''' '''
+today_new=[]
+
+for line in x:
+    today_new.append(json.loads(line))
+
+print(today_new[0])
+'''
+
+
+tommorow = today + yesterday
+sub_total=subcategory_t+subcategory_y
+cou= Counter(sub_total)
+subcategory_yset =set(subcategory_y)
+
+
+for line in tommorow:
+    if line['category'] in cata_y and line['subcategory'] in subcategory_yset :
+        print(line['category'] , ' > ', line['subcategory'],'=',cou[ line['subcategory']])
+        subcategory_yset.remove(line['subcategory'])
+    
+print("   :) __________________________________ :)")
+print()
+    
+
+        
